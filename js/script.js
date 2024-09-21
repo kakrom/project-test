823882fe85a2818d21a032927edea455cfd00d9f
zuxrat <sabirovzuxrat21@gmail.com>
2024-09-21 15:22:50 +0500

    sliders

---
+++ js/script.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const gallery = document.querySelector('.projects-gallery');
+
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    gallery.addEventListener('mousedown', (e) => {
+        isDown = true;
+        gallery.classList.add('active');
+        startX = e.pageX - gallery.offsetLeft;
+        scrollLeft = gallery.scrollLeft;
+    });
+
+    gallery.addEventListener('mouseleave', () => {
+        isDown = false;
+        gallery.classList.remove('active');
+    });
+
+    gallery.addEventListener('mouseup', () => {
+        isDown = false;
+        gallery.classList.remove('active');
+    });
+
+    gallery.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - gallery.offsetLeft;
+        const walk = (x - startX) * 3;
+        gallery.scrollLeft = scrollLeft - walk;
+    });
+});
+document.addEventListener("DOMContentLoaded", () => {
+    const gallery = document.querySelector('.projects-gallery');
+    const prevButton = document.getElementById('prev');
+    const nextButton = document.getElementById('next');
+    const scrollAmount = 500;
+    nextButton.addEventListener('click', () => {
+        gallery.scrollBy({
+            left: scrollAmount,
+            behavior: 'smooth'
+        });
+    });
+    prevButton.addEventListener('click', () => {
+        gallery.scrollBy({
+            left: -scrollAmount,
+            behavior: 'smooth'
+        });
+    });
+});
+document.addEventListener("DOMContentLoaded", () => {
+    const catalogItems = document.querySelector('.catalog-items');
+    const prevButton = document.getElementById('catalog-prev');
+    const nextButton = document.getElementById('catalog-next');
+    const scrollAmount = 500;
+    nextButton.addEventListener('click', () => {
+        catalogItems.scrollBy({
+            left: scrollAmount,
+            behavior: 'smooth'
+        });
+    });
+    prevButton.addEventListener('click', () => {
+        catalogItems.scrollBy({
+            left: -scrollAmount,
+            behavior: 'smooth'
+        });
+    });
+});
