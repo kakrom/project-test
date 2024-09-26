9f8ddaf66b984ba88818302f85a697aef87b1f3e
kakrom <akromkengeshbaev@gmail.com>
2024-09-26 21:56:34 +0500

    js code edit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
-let userName = document.querySelector("#sec11-name").value;
-let phoneNumber = document.querySelector("#sec11-number").value;
-let email = document.querySelector("#sec11-email").value;
-let survey = document.querySelector("#sec11-survey").value;
 let btn = document.querySelector("#sec11-btn");
-let check = document.querySelector("#sec11-check");
 
 btn.addEventListener("click", () => {
-  localStorage.setItem("user", userName);
+  let userName = document.querySelector("#sec11-name").value;
+  let phoneNumber = document.querySelector("#sec11-number").value;
+  let email = document.querySelector("#sec11-email").value;
+  let survey = document.querySelector("#sec11-survey").value;
+
+  let user = {
+    name: userName,
+    phone: phoneNumber,
+    email: email,
+    survey: survey
+  };  
+
+  localStorage.setItem("user", JSON.stringify(user));
 });
