1a34591511fd089c53b4cbd3bfc6b325d24a94d2
Batir <batirshakkh@gmail.com>
2024-10-15 17:52:59 +0500

    header ham footer isteldi

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function menu_click() {
+    const menu_open = document.getElementById('menu_open');
+    
+    menu_open.style.left = '0';
+}
+function menu_close() {
+    const menu_click =document.getElementById('menu_open')
+    menu_click.style.left = '-200%'
+}
